54aec93f1572cec9941aa32bb1f738e2159bc5e7
jsonwamuyu <jysonmuchiri@gmail.com>
2025-03-07 20:55:53 +0300

    change add user

--- frontend/src/components/admin/AddNewUser.tsx
+++ frontend/src/components/admin/AddNewUser.tsx
@@ -1,14 +1,15 @@
 function AddNewUser() {
   return (
-    <div className="w-full my-24" id="create-users">
+    <div className="w-full  my-24" id="create-users">
       <div className="container">
         <h3>Add New User</h3>
-        <form action="" className="flex flex-col gap-8 w-full" >
-          <input type="text" placeholder="Username" />
-          <input type="email" name="" id="" placeholder="Email Address" />
-          {/* <div> */}
+        <form action="" className="flex flex-col gap-8 w-full sm:max-w-md" >
+          <input type="text" placeholder="Username" className="text-sm"/>
+          <input type="email" name="" id="" placeholder="Email Address" className="placeholder:text-sm" />
+          <input type="password" placeholder="Password" className="text-sm"/>
+          <div>
             <button className="cta-primary">Add User</button>
-          {/* </div> */}
+          </div>
         </form>
       </div>
     </div>
